76f8f8718bac6fda6eaf5761bc487c54f6d01944
srcLegend <development@adnan-yilmaz.me>
2019-05-31 17:40:27 -0400

    Clean start

--- tests/helpers.js
+++
@@ -1,9 +0,0 @@
-const hlp = require(`../${global.src_path}/helpers`);
-
-describe('src/helpers', function() {
-  describe('spliceVersion', function() {
-    it('should return a two digit version number', function() {
-      hlp.spliceVersion('1.2.3').should.equal('1.2');
-    });
-  });
-});
