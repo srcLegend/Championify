22ea43304732c8ab529a6502703668f4b28471c2
srcLegend <development@adnan-yilmaz.me>
2019-06-12 16:16:47 -0400

    Porting to C++ - Ongoing

--- source/learn.cpp
+++ source/learn.cpp
@@ -18,19 +18,24 @@ int learn(){
 
 	int var = 15;
 	int *pVar = &var;
-	const char *str = "Hello there!";
-	const char *pStr = str;
+	const char *str = "Hello there!"; // When using C
+	std::string strCpp = "Hello there!"; // When using C++
+	std::string *pStrCpp = &strCpp;
 
-	std::cout << "Value of str                     = " << str << std::endl;
-	std::cout << "Address of str                   = " << &str << std::endl;
-	std::cout << "Value of pStr                    = " << pStr << std::endl;
-	std::cout << "Address of str                   = " << &pStr << std::endl;
+	std::cout << "Value of str                       = " << str << std::endl;
+	std::cout << "Address of str                     = " << &str << std::endl;
 
-	std::cout << "Value of var                     = " << var << std::endl;
-	std::cout << "pVar points to var's address     = " << pVar << std::endl;
-	std::cout << "*pVar points to var's value      = " << *pVar << std::endl;
-	std::cout << "Address of var                   = " << &var << std::endl;
-	std::cout << "Address of pVar                  = " << &pVar << std::endl;
+	std::cout << "Value of strCpp                    = " << strCpp << std::endl;
+	std::cout << "pStrCpp points to strCpp's address = " << pStrCpp << std::endl;
+	std::cout << "*pStrCpp points to strCpp's value  = " << *pStrCpp << std::endl;
+	std::cout << "Address of strCpp                  = " << &strCpp << std::endl;
+	std::cout << "Address of pStrCpp                 = " << &pStrCpp << std::endl;
+
+	std::cout << "Value of var                       = " << var << std::endl;
+	std::cout << "pVar points to var's address       = " << pVar << std::endl;
+	std::cout << "*pVar points to var's value        = " << *pVar << std::endl;
+	std::cout << "Address of var                     = " << &var << std::endl;
+	std::cout << "Address of pVar                    = " << &pVar << std::endl;
 
 	std::ios_base::sync_with_stdio(true);
 	return 0;
--- source/learn.h
+++ source/learn.h
@@ -1,4 +1,3 @@
-#ifndef LEARN_H
-	#define LEARN_H
-	int learn();
-#endif // !LEARN_H
+#pragma once
+
+int learn();
